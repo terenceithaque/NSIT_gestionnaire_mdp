a7ef0be8e26109d41dbecf7638fa55bb8905256a
Terence Le Thierry <terence.lethierry@protonmail.com>
2026-03-21 19:33:12 +0100

    Ajouté une classe BDD

---
+++ bdd/bdd.py
@@ -0,0 +1,8 @@
+"""bdd.py contient une classe BDD représentant une base de données SQLite de mots de passe."""
+import sqlite3
+
+
+class BDD:
+    """Une base de données SQLite contenant des mots de passe."""
+    def __init__(self):
+        pass
