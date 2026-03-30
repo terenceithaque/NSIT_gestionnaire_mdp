8d912455390bc5b701290edb6583e77914d191d5
terenceithaque <96685979+terenceithaque@users.noreply.github.com>
2026-03-30 09:49:50 +0200

    Ajouté la fonction deriver_cle()

--- derivation_cle.py
+++ derivation_cle.py
@@ -4,3 +4,19 @@ import base64
 from cryptography.fernet import Fernet
 from argon2.low_level import hash_secret_raw, Type
 
+def deriver_cle(mdp:str, sel:bytes) -> bytes:
+    """Dérive le mot de passe donné en paramètre en une clé sous forme d'octets."""
+    
+    # Générer la clé à partir du mot de passe et du sel
+    cle = hash_secret_raw(
+        secret=mdp.encode(),
+        salt=sel,
+        time_cost=3,
+        memory_cost=65536,
+        parallelism=1,
+        hash_len=32,
+        type=Type.ID)
+    
+    # Formater la clé en base64
+    return base64.urlsafe_b64encode(cle)
+
